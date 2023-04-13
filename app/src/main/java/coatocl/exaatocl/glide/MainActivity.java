f9b862a54b6f165bc988ed4b522e883338b21c95
digs-h-lakhani2122 <digallakhani21@gmail.com>
2023-04-13 13:23:50 +0530

    Glide-Image-Loader

---
+++ app/src/main/java/coatocl/exaatocl/glide/MainActivity.java
@@ -0,0 +1,35 @@
+package coatocl.exaatocl.glide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView=findViewById(R.id.imageView);
+        button=findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Glide.with(MainActivity.this).load("https://picsum.photos/200").placeholder(R.drawable.ic_launcher_background).error(R.drawable.ic_warning_black_24dp).into(imageView);
+
+            }
+        });
+
+    }
+}
